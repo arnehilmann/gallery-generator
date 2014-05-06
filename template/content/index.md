8cd48e5327525326136e757c426dea775ec5c576
Arne Hilmann <arne@hilmann.de>
2014-05-06 16:37:53 +0200

    initial version

---
+++ template/content/index.md
@@ -0,0 +1,11 @@
+Title: TODO: title
+Date: 
+Category: TODO: category
+Tags: TODO: tags
+Author: TODO: author
+Summary: TODO: summary
+Template: gallery
+url: index.html
+save_as: index.html
+
+
---
+++ template/pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'TODO: author'
+SITENAME = u"TODO: sitename"
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+TAGS_SAVE_AS = ''
+TAG_SAVE_AS = ''
+
+LINKS =  ()
+SOCIAL = ()
+DEFAULT_PAGINATION = False
+RELATIVE_URLS = True
+THEME = "./pelican-bootstrap3/"
+STATIC_PATHS = ["images", "extra/custom.css"]
+CUSTOM_CSS = "static/custom.css"
+EXTRA_PATH_METADATA = {
+    "extra/custom.css": {"path": "static/custom.css"}
+}
